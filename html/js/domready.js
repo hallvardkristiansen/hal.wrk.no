9cebd198cbc754368886826091be08995f6bd0e2
Hallvard Kristiansen <hal@wrk.no>
2015-01-04 22:01:34 +0100

    started coding logo

--- html/js/domready.js
+++ html/js/domready.js
@@ -1,31 +1,38 @@
 $(function() {
-    var iframe = $('#player1')[0];
-    var player = $f(iframe);
-    var status = $('.status');
+  function buildlogo() {
+    var g = d3.select('#logo').append('g');
+    var triangle = g.append('path')
+      .attr('points', '0,0  0,30 30,15')
+      .attr('fill', '#000000');
+  }
+  buildlogo();
+  var iframe = $('#player1')[0];
+  var player = $f(iframe);
+  var status = $('.status');
 
-    // When the player is ready, add listeners for pause, finish, and playProgress
-    player.addEvent('ready', function() {
-        status.text('ready');
-        
-        player.addEvent('pause', onPause);
-        player.addEvent('finish', onFinish);
-        player.addEvent('playProgress', onPlayProgress);
-    });
+  // When the player is ready, add listeners for pause, finish, and playProgress
+  player.addEvent('ready', function() {
+      status.text('ready');
+      
+      player.addEvent('pause', onPause);
+      player.addEvent('finish', onFinish);
+      player.addEvent('playProgress', onPlayProgress);
+  });
 
-    // Call the API when a button is pressed
-    $('button').bind('click', function() {
-        player.api($(this).text().toLowerCase());
-    });
+  // Call the API when a button is pressed
+  $('button').bind('click', function() {
+      player.api($(this).text().toLowerCase());
+  });
 
-    function onPause(id) {
-        status.text('paused');
-    }
+  function onPause(id) {
+      status.text('paused');
+  }
 
-    function onFinish(id) {
-        status.text('finished');
-    }
+  function onFinish(id) {
+      status.text('finished');
+  }
 
-    function onPlayProgress(data, id) {
-        status.text(data.seconds + 's played');
-    }
+  function onPlayProgress(data, id) {
+      status.text(data.seconds + 's played');
+  }
 });
