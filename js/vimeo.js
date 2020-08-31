119c932f68ca00cd80742eff6d3e9cb1e14d2a09
Hallvard Kristiansen <hal@wrk.no>
2020-08-31 20:35:09 +0200

    updated a mangled URL

--- js/vimeo.js
+++ js/vimeo.js
@@ -52,7 +52,7 @@ var vimeo = {
       "tags":"desktop link"
       },{
       "title":"Bultmanns Hoff - online butcher",
-      "description":"Bultmanns Hoff is an ecological beef farm in northern Germany with a webshop where you can order prime beef delivered straight to your door. I built the site based on Wordpress and Woocommerce on commission by <a href='https://www.zentralnorden.com/' target='_blank'>Zentralnorden</a> design agency who provided the designs and are partners in the company.<br \/>\r\n<br \/>\r\nThe site has been taken down, but you can view a case study here: <a href='https://www.<a href='https://www.zentralnorden.com/' target='_blank'>Zentralnorden</a>.com/case-buho' target='blank'>https://www.<a href='https://www.zentralnorden.com/' target='_blank'>Zentralnorden</a>.com/case-buho</a>",
+      "description":"Bultmanns Hoff is an ecological beef farm in northern Germany with a webshop where you can order prime beef delivered straight to your door. I built the site based on Wordpress and Woocommerce on commission by <a href='https://www.zentralnorden.com/' target='_blank'>Zentralnorden</a> design agency who provided the designs and are partners in the company.<br \/>\r\n<br \/>\r\nThe site has been taken down, but you can view a case study here: <a href='https://www.zentralnorden.com/case-buho' target='blank'>https://www.zentralnorden.com/case-buho</a>",
       "image":"project_imgs/bultmannshoff.jpg",
       "link":"https://bultmanns-hoff.de/",
       "width":1280,
